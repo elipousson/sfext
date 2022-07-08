072344b2b3074711b00e51cf48e0def33fbe25f2
Eli Pousson <eli.pousson@gmail.com>
2022-07-07 22:54:42 -0400

    refactor: add list.ok parameter to check_sf

--- R/utils-check.R
+++ R/utils-check.R
@@ -105,13 +105,14 @@ check_logical <- function(x = NULL, arg = caller_arg(x), null.ok = FALSE, n = NU
 #' If x is an `sf` object invisibly return TRUE. If not, return an error with [cli::cli_abort]
 #'
 #' @inheritParams is_sf
+#' @param list.ok If TRUE, return TRUE if x is an sf list or, if ext is TRUE, a list of sf, sfc, or bbox objects. Defaults to FALSE.
 #' @inheritParams cli::cli_abort
 #' @inheritDotParams cli::cli_abort
 #' @export
-check_sf <- function(x, arg = caller_arg(x), null.ok = FALSE, ext = FALSE, call = caller_env(), ...) {
+check_sf <- function(x, arg = caller_arg(x), null.ok = FALSE, list.ok = FALSE, ext = FALSE, call = caller_env(), ...) {
   check_null(x, arg, null.ok)
 
-  if (is_sf(x, ext, null.ok)) {
+  if (is_sf(x, ext, null.ok) | (list.ok && is_sf_list(x, named = FALSE, ext, null.ok))) {
     invisible(return(TRUE))
   }
 
