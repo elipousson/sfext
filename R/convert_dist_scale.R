bbbc4bb3ecd58e464ca4e69e6eb395550f340ef7
Eli Pousson <eli.pousson@gmail.com>
2022-07-11 12:01:42 -0400

    refactor: pass convert_dist_scale ... params to get_paper

--- R/convert_dist_scale.R
+++ R/convert_dist_scale.R
@@ -13,6 +13,8 @@
 #'   e.g. if 1" = 1', the scale factor is 12. optional if scale if provided;
 #'   defaults to `NULL`.
 #' @param dpi dots per square inch (used as conversion factor for "px" to "in")
+#' @inheritParams get_paper
+#' @inheritDotParams get_paper
 #' @return dist values converted from scale_unit to actual_unit based on
 #'   scale_factor or information from standard_scales object. If paper is
 #'   provided, return a data frame with converted distances as actual_width and
@@ -26,13 +28,14 @@ convert_dist_scale <- function(dist = NULL,
                                scale_unit = "in",
                                actual_unit = NULL,
                                scale_factor = NULL,
-                               dpi = 120) {
+                               dpi = 120,
+                               ...) {
   if (!is.null(scale)) {
     scale <- get_scale(scale = scale)
   }
 
   if (!is.null(paper) && is.null(dist)) {
-    paper <- get_paper(paper = paper, orientation = orientation)
+    paper <- get_paper(paper = paper, orientation = orientation, ...)
     dist <- c(paper$width, paper$height)
     scale_unit <- paper$units
   }
