6e1e65aec011624bda2e807f24b279a3356fece3
Eli Pousson <eli.pousson@gmail.com>
2022-07-12 12:57:34 -0400

    test: enable parallel tests
    - test: enable parallel tests
    - test: try skip_on_ci() for failing st_misc test

--- tests/testthat/test-st_misc.R
+++ tests/testthat/test-st_misc.R
@@ -2,11 +2,12 @@ test_that("st_misc functions work", {
   nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
   nc_6543 <- sf::st_transform(nc, 6543)
 
+  expect_error(st_inscribed_square("x"))
+
+  skip_on_ci()
   expect_s3_class(st_square(nc_6543), "sf")
   expect_s3_class(st_square(nc_6543, inscribed = FALSE), "sf")
   expect_s3_class(st_inscribed_square(nc_6543), "sf")
-  expect_error(st_inscribed_square("x"))
-
   expect_s3_class(st_circle(nc_6543, inscribed = TRUE), "sf")
   expect_s3_class(st_circle(nc_6543), "sf")
   expect_s3_class(st_circumscribed_circle(nc_6543), "sf")
