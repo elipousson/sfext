a979c021e351afa068585588d920522bfe7263a4
Eli Pousson <eli.pousson@gmail.com>
2022-08-23 10:17:46 -0400

    feat: allow to arg to take a numeric vector
    -feat: allow to arg to take a numeric vector
    - refactor: update handling of sf to args to avoid map function

--- R/st_nudge.R
+++ R/st_nudge.R
@@ -1,20 +1,22 @@
 #' Nudge a simple feature to the center of another feature and/or a set distance
 #'
 #' Nudge, move, or shift a bbox, sfc, or sf object to the center of another
-#' feature or by a set distance.
+#' feature and/or by a set distance.
 #'
-#' @param nudge_y,nudge_x Distance to nudge geometry in unit. If unit is NULL,
+#' @param to sf object to use as new center for x or length 2 numeric vector
+#'   with the nudge_y and nudge_x distance (in that order).
+#' @param nudge_y,nudge_x Distance to nudge geometry in unit. If unit is `NULL`,
 #'   distance is assumed to be in the same units as the coordinate reference
 #'   system of the input object.
-#' @param unit Units for nudge_y and nudge_x distance.
-#' @param to sf object to use as new center for x
+#' @param unit Units for nudge_y and nudge_x distance (also used if to is
+#'   numeric).
 #' @inheritParams st_scale_rotate
 #' @export
 st_nudge <- function(x,
+                     to = NULL,
                      nudge_y = 0,
                      nudge_x = 0,
                      unit = NULL,
-                     to = NULL,
                      scale = 1,
                      rotate = 0,
                      crs = NULL) {
@@ -37,23 +39,24 @@ st_nudge <- function(x,
   if (is_sf(to, ext = TRUE)) {
     to <- as_sfc(to, crs = crs)
 
-    group_center <- NULL
-
-    if (length(geom) > 1) {
-      group_center <- st_center(sf::st_union(x), "sfc")
+    if (length(geom) != length(to)) {
+      to <- sf::st_union(to)
     }
 
-    geom <-
-      purrr::map_dfr(
-        geom,
-        ~ as_sf(
-          as_sfc(.x, crs = crs) +
-          (st_center(sf::st_union(to), "sfc") - (group_center %||% st_center(.x, "sfc"))),
-          crs = crs
-        )
-      )
+    to <- st_center(to, "sfc")
 
-    geom <- sf::st_geometry(geom)
+    x_center <- st_center(sf::st_union(x), "sfc")
+
+    geom <- sf::st_set_crs(geom + (to - x_center), crs)
+
+  } else if (is.numeric(to) && (length(to) == 2)) {
+    nudge_y <- to[[1]]
+    nudge_x <- to[[2]]
+  } else {
+    cli_abort(
+      "{.arg to} must have a {.cls {c('sf', 'sfc', 'bbox')}} or be a length 2
+      {.cls numeric} vector."
+    )
   }
 
   if (any(is.numeric(c(nudge_y, nudge_x)))) {
@@ -69,8 +72,7 @@ st_nudge <- function(x,
         )
     }
 
-    geom <- geom + nudge
-    geom <- as_sfc(geom, crs)
+    geom <- sf::st_set_crs(geom + nudge, crs)
   }
 
   if (is_x_sfc) {
