0670946cdb30f02850d5642bda8d0a5c6a363bab
Eli Pousson <eli.pousson@gmail.com>
2022-08-24 21:36:31 -0400

    feat: revise compare_dist args to expand options
    - feat: revise compare_dist args by dropping check and round and adding how and tolerance to allow more types of comparisons
    - refactor: move dist argument before x argument (feels more intuitive based on function name)

--- R/compare_dist.R
+++ R/compare_dist.R
@@ -1,9 +1,15 @@
-#' Does this distance fit within the bounding box of x or another distance?
+#' Compare a distance to the dimension of a bounding box or another distance
+#' value
 #'
-#' Check if a distance fits within the bounding box of a sf, sfc, or bbox
-#' object, a distance returned by [sf::st_distance()] passing x as the first
-#' parameter, or a units class or numeric value. Set `drop = FALSE` to return
-#' the number of times dist fits within the specified distance.
+#' Compare dist to a distance based on x. If to is "xdist" (default), "ydist",
+#' "diagdist", or any length 2 character vector supported by [sf_bbox_dist()]
+#' dist is compared to the resulting distance. If to is an sf or sfc object, the
+#' comparison distance is the results from [sf::st_distance()] with x as the
+#' first parameter and to as the second. x can also be a numeric value with or
+#' without a units class. Use the "how" argument to determine the type of
+#' comparison: "ratio" (dist divided by comparison distance) "fit" (how many
+#' lengths of dist can fit wholly within the comparison distance) "longer",
+#' "shorter", "same" (uses tolerance parameter).
 #'
 #' @param x A `sf`, `sfc`, or `bbox` object or a numeric vector.
 #' @param dist Distance to check as a units class or numeric vector
@@ -17,68 +23,63 @@
 #'   an sf or sfc object, to and any additional ... parameters are passed to
 #'   [sf::st_distance()] using x as the first argument and to as the second. If
 #'   x is a numeric vector the value of to is ignored.
-#' @param check,round If check is `TRUE` (default), return a logical vector
-#'   indicating if the value dist can fit within the distance specified by x and
-#'   to. If check is `FALSE` and round is `TRUE`, return the number of times,
-#'   dist fits in the specified distance or 0 if dist is greater than the
-#'   specified distance. If check is `FALSE` and round is `FALSE`, return the
-#'   comparison distance divided by the input distance.
+#' @param what If "ratio", dist divided by the comparison distance. If what is
+#'   "fit", return the number of times dist fits in the comparison distance
+#'   without remainder. If "longer", "shorter", "same", return a logical vector.
 #' @family dist
 #' @returns A logical or numeric vector.
 #' @export
 #' @importFrom rlang arg_match
 #' @importFrom sf st_distance
-compare_dist <- function(x,
-                         dist = NULL,
+compare_dist <- function(dist,
+                         x,
                          units = NULL,
                          to = "xdist",
-                         check = TRUE,
-                         round = TRUE,
+                         how = "ratio",
+                         tolerance = 1.5e-8,
                          ...) {
   # Modify value of to based on x (does not warn when overriding existing to value)
-  to <-
+  compare_to <-
     dplyr::case_when(
       is.numeric(x) ~ "x",
-      (length(to) == 2) && is.character(to) ~ "bbox",
       is_sf(to, ext = TRUE) ~ "dist",
-      TRUE ~ to
+      (length(to) == 2) && is.character(to) ~ "bbox",
+      (length(to) == 1) && is.character(to) ~ to
     )
 
   if (is_sf(to) | is_sfc(to)) {
-    compare <- "x"
     x <- sf::st_distance(x, y = to, ...)
+    compare_to <- "x"
   }
 
-  if (compare == "x") {
+  compare_to <-
+    arg_match(compare_to, c("xdist", "ydist", "diagdist", "bbox", "x"))
+
+  if (compare_to == "x") {
     x <- convert_dist_units(x, to = units, drop = TRUE)
   } else if (!is_bbox(x)) {
     x <- as_bbox(x)
   }
 
-  compare <-
-    arg_match(compare, c("xdist", "ydist", "diagdist", "bbox", "x"))
-
-
-  compare_dist <-
-    switch(compare,
-      "xdist" = sf_bbox_xdist(x, units = units),
-      "ydist" = sf_bbox_ydist(x, units = units),
-      "diagdist" = sf_bbox_diagdist(x, units = units),
-      "bbox" = sf_bbox_dist(x, to = to, units = units, ...),
+  dist_to_compare <-
+    switch(compare_to,
+      "xdist" = sf_bbox_xdist(x, units = units, drop = TRUE),
+      "ydist" = sf_bbox_ydist(x, units = units, drop = TRUE),
+      "diagdist" = sf_bbox_diagdist(x, units = units, drop = TRUE),
+      "bbox" = sf_bbox_dist(x, to = to, units = units, drop = TRUE, ...),
       "x" = x
     )
 
   dist <- convert_dist_units(dist, to = units, drop = TRUE)
 
-  if (check) {
-    return(dist / compare_dist <= 1)
-  }
+  how <- arg_match(how, c("ratio", "fit", "longer", "shorter", "same"))
 
-  compare <- compare_dist / dist
-
-  if (round) {
-    return(round(compare, 0))
-  }
-
-  compare
+  switch (how,
+    "ratio" = dist / dist_to_compare,
+    "fit" = round(dist / dist_to_compare, 0),
+    "longer" = dist > dist_to_compare,
+    "shorter" = dist < dist_to_compare,
+    "same" = all.equal(dist_to_compare, dist, tolerance = tolerance)
+  )
 }
+
