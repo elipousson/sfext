5f3fb4ec4c3dd0b4dfe9911c5b28e03e72b9298a
Eli Pousson <eli.pousson@gmail.com>
2022-09-06 15:36:09 -0400

    refactor: import rlang is_interactive

--- R/sfext-package.R
+++ R/sfext-package.R
@@ -1,5 +1,5 @@
 #' @import rlang
-#' @importFrom rlang .data .env caller_env has_length
+#' @importFrom rlang .data .env caller_env has_length is_interactive
 #' @importFrom cli cli_abort cli_warn cli_inform
 #' @importFrom glue glue glue_collapse
 #' @keywords internal
@@ -7,4 +7,4 @@
 
 ## usethis namespace: start
 ## usethis namespace: end
-on_load(local_use_cli())
+rlang::on_load(rlang::local_use_cli())
