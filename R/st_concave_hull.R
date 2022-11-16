b6b08f0a5de174a866f75c2d72946a3e09fb4731
Eli Pousson <eli.pousson@gmail.com>
2022-11-15 22:05:17 -0500

    feat: add new st_concave_hull function

---
+++ R/st_concave_hull.R
@@ -0,0 +1,54 @@
+#' Make a concave hull around simple feature object by attribute
+#'
+#' @param x A sf object.
+#' @param by Column name to use for grouping and combining geometry, Default:
+#'   `NULL`
+#' @param centroid If `TRUE`, use centroids for geometry of x, Default: `FALSE`
+#' @inheritParams concaveman::concaveman
+#' @seealso
+#'  \code{\link[concaveman]{concaveman}}
+#' @rdname st_concave_hull
+#' @export
+#' @importFrom sf st_centroid st_combine st_cast st_make_valid st_set_geometry
+#' @importFrom dplyr summarise
+#' @importFrom purrr map_dfr
+st_concave_hull <- function(x,
+                            by = NULL,
+                            centroid = FALSE,
+                            concavity = 2,
+                            length_threshold = 0) {
+  is_pkg_installed("concaveman")
+
+  check_sf(x)
+
+  if (centroid) {
+    x <- suppressMessages(sf::st_centroid(x))
+  }
+
+  if (!is.null(by)) {
+    sf_col <- get_sf_col(x)
+    x <-
+      dplyr::summarise(
+        group_by(x, .data[[by]]),
+        "{sf_col}" := sf::st_combine(.data[[sf_col]])
+      )
+  }
+
+  if (!is_multipoint(x)) {
+    x <- sf::st_cast(x, to = "MULTIPOINT")
+  }
+
+  geometry <-
+    purrr::map_dfr(
+      as_sfc(x),
+      ~ sf::st_make_valid(
+        concaveman::concaveman(
+          points = as_sf(.x),
+          concavity = concavity,
+          length_threshold = length_threshold)
+      )
+    )
+
+
+  sf::st_set_geometry(x, as_sfc(geometry))
+}
