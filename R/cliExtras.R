5061cd17bf7ddb89687146de770d3657352e6fc0
Eli Pousson <eli.pousson@gmail.com>
2022-11-26 00:38:36 -0500

    refactor: move cli_paths to burgled script file

--- R/cliExtras.R
+++ R/cliExtras.R
@@ -52,3 +52,12 @@
   cli_inform(c(`>` = message, ...), .envir = .envir)
   readline(prompt = prompt)
 }
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# cli_paths (copied from cliExtras:::cli_paths)
+# from cliExtras 0.1.0
+`cli_paths` <- function(path, ..., .envir = parent.frame()) {
+  len_path <- length(path)
+  cli_inform(c(v = paste0(..., " {len_path} file{?s}:")), .envir = .envir)
+  path <- paste0("{.file ", path, "}")
+  cli::cli_bullets(rlang::set_names(path, rep("*", len_path)))
+}
