b5bf937081d9261b466a2fae727e97b0d5b263ab
Eli Pousson <eli.pousson@gmail.com>
2022-11-27 00:35:03 -0500

    test: skip number_features on CI

--- tests/testthat/test-number_features.R
+++ tests/testthat/test-number_features.R
@@ -1,13 +1,13 @@
 test_that("number_features works", {
   nc <- read_sf_path(system.file("shape/nc.shp", package = "sf"))
 
+  skip_on_ci()
   expect_s3_class(
     number_features(
       nc
     ),
     "sf"
   )
-
   expect_s3_class(
     number_features(
       nc,
@@ -16,7 +16,6 @@ test_that("number_features works", {
     ),
     "sf"
   )
-
   expect_equal(
     rlang::has_name(
       number_features(
