f17a3ad908071e630eb8ab809c4a0cbff77f8266
Eli Pousson <eli.pousson@gmail.com>
2022-11-27 22:21:13 -0500

    feat(conver_dist_scale): add standard + series parameters
    - feat(conver_dist_scale): add standard + series parameters + improved checks + warning messages

--- R/convert_dist_scale.R
+++ R/convert_dist_scale.R
@@ -4,15 +4,17 @@
 #' This function converts scale distances to actual units based on named
 #' [standard_scales].
 #'
-#' @param dist distance to convert. If paper is provided, paper width and height
-#'   are used as dist.
+#' @param dist distance to convert. If paper is provided, dist is optional and
+#'   paper width and height are used as dist.
 #' @inheritParams get_scale
+#' @param scale_standard,scale_series Passed to standard and scale parameters of
+#'   [get_scale()].
 #' @param scale_unit "mm" (converted to cm by dividing by 10), "cm", "px"
 #'   (converted to inches by dividing by dpi), or "in".
-#' @param actual_unit any unit supported by [convert_dist_units()]
 #' @param scale_factor factor for converting from scale_unit to actual_unit,
 #'   e.g. if 1" = 1', the scale factor is 12. optional if scale if provided;
 #'   defaults to `NULL`.
+#' @param actual_unit any unit supported by [convert_dist_units()]
 #' @param dpi dots per square inch (used as conversion factor for "px" to "in")
 #' @inheritParams get_paper
 #' @inheritDotParams get_paper
@@ -25,21 +27,51 @@
 #' @family dist
 #' @export
 convert_dist_scale <- function(dist = NULL,
+                               scale = NULL,
+                               scale_standard = NULL,
+                               scale_series = NULL,
+                               scale_unit = "in",
+                               scale_factor = NULL,
+                               actual_unit = NULL,
+                               dpi = 120,
                                paper = NULL,
                                orientation = NULL,
-                               scale = NULL,
-                               scale_unit = "in",
-                               actual_unit = NULL,
-                               scale_factor = NULL,
-                               dpi = 120,
                                ...) {
-  if (!is.null(scale)) {
-    scale <- get_scale(scale = scale)
+  if (is.character(scale) && has_length(scale, 1)) {
+    scale_nm <- scale
+    scale <- get_scale(scale = scale, standard = standard, series = series)
+
+    cli_abort_ifnot(
+      c("{.arg scale} {.val {scale_nm}} returned {nrow(scale)} scales from {.code standard_scales}.",
+        "i" = "Provide {.arg standard} and {.arg series} parameters to return only 1 scale."
+      ),
+      condition = nrow(scale) == 1
+    )
+
+    cli_warn_ifnot(
+      c("{.arg actual_unit} is ignored if {.arg scale} is provided."),
+      condition = is.null(actual_unit)
+    )
   }
 
   if (!is.null(paper) && is.null(dist)) {
+    paper_nm <- paper
     paper <- get_paper(paper = paper, orientation = orientation, ...)
+
+    cli_abort_ifnot(
+      c("{.arg paper} {.val {paper_nm}} returned {nrow(paper)} options from {.code paper_sizes}.",
+        "i" = "Provide {.arg orientation} or other parameters for {.fn get_paper} to return 1 option."
+      ),
+      condition = nrow(paper) == 1
+    )
+
     dist <- c(paper$width, paper$height)
+
+    cli_warn_ifnot(
+      c("{.arg scale_unit} and {.arg scale_factor} are ignored if {.arg paper} is provided."),
+      condition = is.null(scale_unit) | is.null(scale_factor) | (!is.null(scale_unit) && scale_unit == paper$units)
+    )
+
     scale_unit <- paper$units
   }
 
@@ -59,11 +91,8 @@ convert_dist_scale <- function(dist = NULL,
   }
 
   if (is.data.frame(scale)) {
-    actual_unit <-
-      scale[[paste0("scale_", scale_unit, "_unit")]]
-
-    scale_factor <-
-      scale[[paste0("scale_", scale_unit)]]
+    actual_unit <- scale[[paste0("scale_", scale_unit, "_unit")]]
+    scale_factor <- scale[[paste0("scale_", scale_unit)]]
   } else {
     stopifnot(
       is.numeric(scale_factor),
