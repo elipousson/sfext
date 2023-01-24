b445a7be30026b03353e516c95e077c618d1d1e6
Eli Pousson <eli.pousson@gmail.com>
2023-01-24 00:15:51 -0500

    feat(sf_bbox_corners): allow sf or sfc class outputs

--- R/sf_bbox_corners.R
+++ R/sf_bbox_corners.R
@@ -8,38 +8,57 @@
 #'   feature
 #' @param .id A name to use for added column with cardinal position of points,
 #'   Default: 'corner'
-#' @return A `sf` with four features or a length 4 `sfc` object with POINT
+#' @param class Class of object to return. Must be "sf" (default) or "sfc".
+#' @return A `sf` with four features (duplicating input feature with new
+#'   geometry derived from bounding box) or a length 4 `sfc` object with POINT
 #'   geometry.
 #' @export
 #' @importFrom sf st_cast st_drop_geometry
 #' @importFrom dplyr bind_cols
-sf_bbox_corners <- function(x, .id = "corner") {
+sf_bbox_corners <- function(x, .id = "corner", class = "sf") {
   check_sf(x, ext = TRUE)
+  class <- arg_match(class, c("sf", "sfc"))
 
   bbox <- x
+  sf_in <- FALSE
 
   if (!is_bbox(x)) {
+    if (is_sf(x)) {
+      sf_in <- TRUE
+    }
+
+    cli_abort_ifnot(
+      "{.arg x} must be a length 1 {.cls sfc} object or a {.cls sf} object with 1 feature.",
+      condition = (is_sf(x) && (nrow(x) == 1)) | (is_sfc(x) && length(x) == 1)
+    )
+
     bbox <- as_bbox(x)
   }
 
-  cli_abort_ifnot(
-    "{.arg x} must be a length 1 {.cls sfc} object or a {.cls sf} object with 1 feature.",
-    condition = (is_sf(x) && (nrow(x) == 1)) | (is_sfc(x) && length(x) == 1)
-  )
+  geometry <- sf::st_cast(as_sfc(bbox), to = "POINT")[1:4]
 
-  geometry <- sf::st_cast(as_sfc(bbox), to = "POINT")[1:4, ]
-
-  if (!is_sf(x)) {
+  if (class != "sf") {
     return(geometry)
   }
 
-  x <- has_same_name_col(x, .id)
+  if (sf_in) {
+    x <- has_same_name_col(x, .id)
+
+    return(
+      as_sf(
+        dplyr::bind_cols(
+          sf::st_drop_geometry(x),
+          "{.id}" := c("SW", "SE", "NE", "NW"),
+          "geometry" = geometry
+        )
+      )
+    )
+  }
 
   as_sf(
     dplyr::bind_cols(
-      sf::st_drop_geometry(x),
       "{.id}" := c("SW", "SE", "NE", "NW"),
-      geometry
+      "geometry" = geometry
     )
   )
 }
