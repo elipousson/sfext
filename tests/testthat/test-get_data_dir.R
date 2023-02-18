43f97918c77d85021006484c486b44ac41286f65
Eli Pousson <eli.pousson@gmail.com>
2023-02-18 01:49:01 -0500

    test: add get_data_dir tests

---
+++ tests/testthat/test-get_data_dir.R
@@ -0,0 +1,26 @@
+test_that("get_data_dir works", {
+  expect_true(
+    is.null(get_data_dir())
+  )
+  expect_true(
+    str_detect(get_data_dir(cache = TRUE), "sfext$")
+  )
+})
+
+test_that("get_path_filetype works", {
+  expect_true(
+    get_path_filetype(filetype = "jpeg") == "jpeg"
+  )
+})
+
+test_that("get_data_dir warns and errors", {
+  expect_error(
+    get_data_dir(null.ok = FALSE)
+  )
+  expect_warning(
+    get_data_dir(
+      path = "xyz",
+      create = FALSE
+    )
+  )
+})
