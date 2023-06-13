a478c9125253c2141159db4874d6a3f012d59b3f
Eli Pousson <eli.pousson@gmail.com>
2023-06-13 13:58:30 -0400

    test: break up read_sf_ext test + add read_sf_pkg test

--- tests/testthat/test-read_sf_ext.R
+++ tests/testthat/test-read_sf_ext.R
@@ -1,11 +1,4 @@
-test_that("read_sf_ext works", {
-  skip_on_ci()
-  expect_s3_class(
-    read_sf_ext(
-      url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NapervilleShelters/FeatureServer/0"
-    ),
-    "sf"
-  )
+test_that("read_sf_ext works with file paths", {
 
   nc <- read_sf_ext(
     path = system.file("shape/nc.shp", package = "sf")
@@ -38,39 +31,15 @@ test_that("read_sf_ext works", {
     ),
     "sf"
   )
+})
 
+test_that("read_sf_ext works with mapbaltimore package", {
+  skip_if_not_installed("mapbaltimore")
   expect_s3_class(
     read_sf_ext(
-      url = "https://gist.githubusercontent.com/elipousson/77271144b3cb82efd732c4e30b273c5a/raw/68dc6008fe27cb7047aa1c5dfba1fa17f6f88f16/baltimore_city_tracts_acs5_2019_B02001_003.geojson"
+      "trees",
+      package = "mapbaltimore"
     ),
     "sf"
-  )
-
-  expect_s3_class(
-    read_sf_ext(
-      url = "https://www.google.com/maps/d/u/0/viewer?mid=1CEssu_neU7lx_vAZs5qpufOBoUQ&ll=-3.81666561775622e-14%2C0&z=1"
-    ),
-    "sf"
-  )
-
-  # skip_if_offline("opendata.baltimorecity.gov/egis/rest/services/")
-  expect_s3_class(
-    read_sf_ext(
-      url = "https://opendata.baltimorecity.gov/egis/rest/services/Hosted/311_Customer_Service_Requests_2020_csv/FeatureServer/0",
-      name = "Abell",
-      name_col = "neighborhood",
-      coords = c("longitude", "latitude")
-    ),
-    "sf"
-  )
-
-  expect_s3_class(
-    read_sf_ext(
-      url = "https://opendata.baltimorecity.gov/egis/rest/services/Hosted/311_Customer_Service_Requests_2020_csv/FeatureServer/0",
-      name = "Abell",
-      name_col = "neighborhood",
-      coords = NULL
-    ),
-    "data.frame"
   )
 })
