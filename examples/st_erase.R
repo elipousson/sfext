fd54df43d4a70a7c91d450bad82b6df3b680208c
Eli Pousson <eli.pousson@gmail.com>
2024-10-08 21:31:45 -0400

    Fix st_erase example (add missing sf prefixes)

--- examples/st_erase.R
+++ examples/st_erase.R
@@ -4,7 +4,7 @@ nc <- st_transform_ext(nc, 3657)
 
 plot(
   st_erase(
-    st_buffer(nc[1, ], 1000),
+    sf::st_buffer(nc[1, ], 1000),
     nc[1, ]
   ),
   max.plot = 1
@@ -13,7 +13,7 @@ plot(
 plot(
   st_trim(
     nc,
-    st_buffer(nc[1, ], 2000)
+    sf::st_buffer(nc[1, ], 2000)
   ),
   max.plot = 1
 )
