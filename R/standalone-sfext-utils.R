10f82fce447c632d353e6be412671f8e17e845bb
Eli Pousson <eli.pousson@gmail.com>
2024-10-08 21:22:00 -0400

    Correct multiple issues w/ standalone sfext utils
    * Correct typos for allow_null argument.
    * Correct typo in `.bbox_transform()` function (reference to non-existent bbox argument instead of x).
    * Use standard `sf::st_transform()` to transform `bbox` if sf (>=1.0-17) is installed.
    * Remove `.sf_point_on_surface()` and `.sf_centroid()` functions and add replacement `.sf_point()` function.
    * Add helper `.st_fn()` function.

--- R/standalone-sfext-utils.R
+++ R/standalone-sfext-utils.R
@@ -1,54 +1,55 @@
 # ---
 # repo: elipousson/sfext
 # file: standalone-sfext-utils.R
-# last-updated: 2023-09-08
+# last-updated: 2024-10-08
 # license: https://creativecommons.org/publicdomain/zero/1.0/
 # imports: [cli, rlang, sf, units]
 # ---
 #
 # This file provides a set of helper functions for check the class of sf objects
-# and related object types and wrappers for key `{sf}` functions that suppress
+# and related object types and wrappes for key `{sf}` functions that suppress
 # excess warnings and allow NULL inputs
 #
 # ## Changelog
+#
+# 2024-10-08:
+# * Correct typos for allow_null argument.
+# * Correct typo in `.bbox_transform()` function (reference to non-existent bbox argument instead of x).
+# * Use standard `sf::st_transform()` to transform `bbox` if sf (>=1.0-17) is installed.
+# * Remove `.sf_point_on_surface()` and `.sf_centroid()` functions and add replacement `.sf_point()` function.
+# * Add helper `.st_fn()` function.
 #
 # 2023-09-08:
 # * First version of simple helpers and wrappers.
 #
 # nocov start
 
+# Is x a sfg object?
 .is_sfg <- function(x) {
   inherits(x, "sfg")
 }
 
+# Is x a sfc object?
 .is_sfc <- function(x) {
   inherits(x, "sfc")
 }
 
+# Is x a sf object?
 .is_sf <- function(x) {
   inherits(x, "sf")
 }
 
-.is_sfish <- function(x, class = c("sfc", "sfg")) {
-  inherits(x, c("sf", class))
-}
-
+# Is x a bbox object?
 .is_bbox <- function(x) {
   inherits(x, "bbox")
 }
 
+# Is x a units object?
 .is_units <- function(x) {
   inherits(x, "units")
 }
 
-.cli_vec_or <- function(x,
-                        style = c(
-                          cli::builtin_theme()$span.val,
-                          "vec-last" = " or "
-                        )) {
-  cli::cli_vec(nm, style = style)
-}
-
+# Check if x is a sf object or another allowed class
 .check_sf <- function(x,
                       ...,
                       allow_empty = TRUE,
@@ -61,7 +62,7 @@
     return(x)
   }
 
-  if (!allow_empty && .is_sfish(x) && sf::st_is_empty(x)) {
+  if (!allow_empty && inherits_any(x, c("sf", "sfc")) && any(sf::st_is_empty(x))) {
     message <- "{.arg {arg}} can't contain any empty feature geometries."
     cli::cli_abort(message, ..., call = call)
   }
@@ -80,13 +81,14 @@
   not a {.obj_simple_type {x}}"
 
   if (length(allow_class) > 1) {
-    message <- "{.arg {arg}} must be a {.cli_vec_or(allow_class)} object,
+    message <- "{.arg {arg}} must be a {.or {allow_class}} object,
     not a {.obj_simple_type {x}}"
   }
 
   cli::cli_abort(message, ..., call = call)
 }
 
+# Check if x is a sf, sfc, or bbox object
 .check_sfish <- function(x,
                          ...,
                          allow_empty = TRUE,
@@ -107,6 +109,7 @@
   )
 }
 
+# Tranform a sf, sfc, or bbox object to a supplied CRS
 .sf_transform <- function(x,
                           crs = NULL,
                           ...,
@@ -116,62 +119,102 @@
   .check_sfish(
     x,
     arg = arg,
-    allow_null = alllow_null,
+    allow_null = allow_null,
     allow_bbox = TRUE,
     call = call
   )
-
-  if (.is_bbox(x)) {
-    .bbox_transform(x, crs = crs, ...)
-  }
 
   if (is.null(x) || is.null(crs)) {
     return(x)
   }
 
+  if (.is_bbox(x)) {
+    return(.bbox_transform(x, crs = crs, ..., arg = arg, call = call))
+  }
+
   sf::st_transform(x, crs = sf::st_crs(crs), ...)
 }
 
+
+# Tranform a bbox object to a supplied CRS
 .bbox_transform <- function(x, crs = NULL, ..., allow_null = TRUE) {
   if (allow_null && (is_null(crs) || is_null(x))) {
     return(x)
   }
 
-  sf::st_bbox(sf::st_transform(sf::st_as_sfc(bbox), crs = sf::st_crs(crs), ...))
+  if (is_installed("sf", version = "1.0-17")) {
+    return(sf::st_transform(x, crs = crs))
+  }
+
+  sf::st_bbox(sf::st_transform(sf::st_as_sfc(x), crs = sf::st_crs(crs), ...))
 }
 
-.sf_centroid <- function(x,
-                         ...,
-                         crs = NULL,
-                         allow_null = TRUE,
-                         arg = caller_arg(x),
-                         call = caller_env()) {
-  .check_sfish(x, arg = arg, allow_null = alllow_null, call = call)
+# Convert a sf, sfc, or bbox object to a data frame of coordinates
+.sf_coords <- function(x,
+                       ...,
+                       coords = c("lon", "lat"),
+                       placement = c("surface", "centroid"),
+                       .f = NULL,
+                       crs = 4326,
+                       allow_null = TRUE,
+                       arg = caller_arg(x),
+                       call = caller_env()) {
+  x <- .sf_point(
+    x = x,
+    placement = placement,
+    .f = .f,
+    ...,
+    crs = crs,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
+
+  set_names(
+    as.data.frame(sf::st_coordinates(x)),
+    coords
+  )
+}
+
+# Convert a sf or sfc object to POINT geometry with a supplied placement or
+# function
+.sf_point <- function(x,
+                      ...,
+                      placement = c("surface", "centroid"),
+                      .f = NULL,
+                      crs = NULL,
+                      allow_null = TRUE,
+                      arg = caller_arg(x),
+                      call = caller_env()) {
+  if (is.null(.f)) {
+    .f <- switch(placement,
+      surface = sf::st_point_on_surface,
+      centroid = sf::st_centroid
+    )
+  }
+
+  .sf_fn(x = x, .f = .f, ..., crs = crs, allow_null = allow_null, arg = arg, call = call)
+}
+
+# Apply a spatial transformation function while suppressing warnings
+.sf_fn <- function(x,
+                   ...,
+                   .f = sf::st_centroid,
+                   crs = NULL,
+                   allow_null = TRUE,
+                   arg = caller_arg(x),
+                   call = caller_env()) {
+  .check_sfish(x, arg = arg, allow_null = allow_null, call = call)
 
   if (is.null(x)) {
     return(x)
   }
 
-  x <- suppressWarnings(sf::st_centroid(x, ...))
-  .sf_transform(x, crs = crs, allow_null = allow_null)
+  x <- suppressWarnings(.f(x, ...))
+  .sf_transform(x, crs = crs, allow_null = allow_null, arg = arg, call = call)
 }
 
-.sf_point_on_surface <- function(x,
-                                 ...,
-                                 crs = NULL,
-                                 allow_null = TRUE,
-                                 arg = caller_arg(x),
-                                 call = caller_env()) {
-  .check_sfish(x, arg = arg, allow_null = alllow_null, call = call)
-
-  if (is.null(x)) {
-    return(x)
-  }
-
-  x <- suppressWarnings(sf::st_point_on_surface(x, ...))
-  .sf_transform(x, crs = crs, allow_null = allow_null)
-}
-
+# Apply a buffer
 .sf_buffer <- function(x,
                        dist = NULL,
                        unit = NULL,
@@ -180,7 +223,7 @@
                        allow_null = TRUE,
                        arg = caller_arg(x),
                        call = caller_env()) {
-  .check_sfish(x, arg = arg, allow_null = alllow_null, call = call)
+  .check_sfish(x, arg = arg, allow_null = allow_null, call = call)
 
   if (is.null(x)) {
     return(x)
@@ -194,7 +237,7 @@
     x <- sf::st_buffer(x, dist = dist, ...)
   }
 
-  .sf_transform(x, crs = crs)
+  .sf_transform(x, crs = crs, arg = arg, call = call)
 }
 
 # nocov end
